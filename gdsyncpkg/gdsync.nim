c8f32544a54608ec031b224b691e13c49133f15f
ryancotter <ryancotter@gitlab.com>
2017-05-07 17:51:38 +0800

    Updates using google drive api

--- gdsyncpkg/gdsync.nim
+++ gdsyncpkg/gdsync.nim
@@ -1,35 +1,79 @@
 import oauth2
+import typetraits
 import strutils, httpclient
 import json
+import asyncnet, asyncdispatch
 include secret.auth
 
-const 
-  authorizeURL = "https://accounts.google.com/o/oauth2/v2/auth"
-  accessTokenUrl = "https://accounts.google.com/o/oauth2/v2/token"
 
-proc main*(argv: seq[string] = nil): int =
+proc main(argv: seq[string] = nil) {.async.} =
   ## Google Drive Syncer main function
   var clientID = OAUTH_CLIENT_ID
   var clientSecret = OAUTH_SECRET
-  
-  let html = "resources/index.html".readFile()
-  
+
+  var client = newAsyncHttpClient()
+  let openid_json = await client.getContent("https://accounts.google.com/.well-known/openid-configuration")
+
+  echo openid_json
+  let openid_urls = parseJson(openid_json)
+
+  let authorizeURL = openid_urls["authorization_endpoint"]
+  let accessTokenUrl = openid_urls["token_endpoint"]
+  let html = "gdsyncpkg/resources/index.html".readFile()
+  var gapiGetUrl = "https://www.googleapis.com/drive/v3/files/"
+  var export_url_part = "/export?mimeType="
+  echo authorizeURL
+  echo accessTokenUrl
+
   let response = authorizationCodeGrant(
-      authorizeUrl,
-      accessTokenUrl,
+      getStr(authorizeURL),
+      getStr(accessTokenUrl),
       clientId,
       clientSecret,
       html,
       scope = @["https://www.googleapis.com/auth/drive",
       "https://www.googleapis.com/auth/drive.metadata"])
 
+  echo response.body
+
+  let
+    obj = parseJson(response.body)
+    url = "https://www.googleapis.com/drive/v3/files"
+    accessToken = obj["access_token"].str
+    tokenType = obj["token_type"].str
+    refreshToken = obj["refresh_token"].str
+
+  echo obj
+
+  if tokenType == "Bearer":
+    let r = bearerRequest(url, accessToken)
+    let file_json = parseJson(r.body)
+    var id_list: seq[string] = @[]
+    var name_list: seq[string] = @[]
+    var media_list: seq[string] = @[]
+    var file_list = file_json["files"]
+    for file in file_list:
+      echo file
+      if getStr(file["kind"]) == "drive#file":
+        id_list.add(getStr(file["id"]))
+        name_list.add(getStr(file["name"]))
+        media_list.add(getStr(file["mimeType"]))
+    echo id_list
+    echo name_list
+
+    for media, id in [media_list, id_list].pairs:
+      echo id.type.name
+      echo media.type.name
+      #var id_url = gapiGetUrl & id & export_url_part & media
+      #echo id_url
+      #let id_r = bearerRequest(id_url, accessToken)
+      #echo id_r
 
   if argv == nil:
     echo "No Commands"
-    return -1
 
   echo argv
-  return 1
+  quit()
 
 when isMainModule:
   import os
@@ -39,5 +83,5 @@ when isMainModule:
   let argv = if paramCount() > 0: commandLineParams()
             else: nil
 
-  echo main(argv)
-
+  asyncCheck main(argv)
+  runForever()
