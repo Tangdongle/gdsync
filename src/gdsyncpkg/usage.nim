0780d7faa6df3f7b426f3ee6b0647c47e7254d6d
Ashley Broughton <a.broughton.en@gmail.com>
2017-05-09 07:31:34 +0200

    Flesh out command line parsing and add basic usage.

---
+++ src/gdsyncpkg/usage.nim
@@ -0,0 +1,17 @@
+const usage* = """
+Usage: gdsync COMMAND [opts]
+
+Commands:
+  Not applicable at this time.
+
+
+Options:
+  Not applicable at this time.
+
+
+For more information read the Github readme:
+  https://github.com/gdsync/gdsync#readme
+"""
+
+proc showUsage*() =
+  echo(usage)
